4284fd5d9936977b953f60fa6194888fea0f6fe9
Oksana <oksanashukh@gmail.com>
2018-08-11 18:15:18 +0300

    adding sass file

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('app/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('app/css/main.css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('app/sass/*.scss', ['sass']);
+});
+
+
+gulp.task("default", function () {
+  gulp.start("sass");
 });
